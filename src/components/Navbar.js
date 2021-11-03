9a10426935ccd13c6c1a69f546f13fc5c892a1d2
Stanislav Iarotchi <stanislav.iarotchi@amdaris.com>
2021-11-03 14:28:21 +0200

    Refactor adaptive arientation and platformsx

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,11 +1,19 @@
 import React from "react";
-import { StyleSheet, View } from "react-native";
+import { StyleSheet, View, Platform } from "react-native";
 import { THEME } from "../theme";
 import { AppTextBold } from "./ui/AppTextBold";
 
 export const Navbar = ({ title }) => {
   return (
-    <View style={styles.navbar}>
+    <View
+      style={[
+        styles.navbar,
+        Platform.select({
+          ios: styles.navbarIos,
+          android: styles.navbarAndroid,
+        }),
+      ]}
+    >
       <AppTextBold style={styles.text}>{title}</AppTextBold>
     </View>
   );
@@ -14,13 +22,19 @@ export const Navbar = ({ title }) => {
 const styles = StyleSheet.create({
   navbar: {
     height: 70,
-    backgroundColor: THEME.MAIN_COLOR,
     alignItems: "center",
     justifyContent: "flex-end",
     paddingBottom: 10,
   },
+  navbarAndroid: {
+    backgroundColor: THEME.MAIN_COLOR,
+  },
+  navbarIos: {
+    borderBottomColor: THEME.MAIN_COLOR,
+    borderBottomWidth: 1,
+  },
   text: {
-    color: "white",
+    color: Platform.OS === "ios" ? THEME.MAIN_COLOR : "white",
     fontSize: 20,
   },
 });
